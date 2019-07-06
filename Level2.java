e78e23fc7537eb992587021975db2bf08cf02735
JoacoGDR <45571740+JoacoGDR@users.noreply.github.com>
2019-07-06 04:21:46 -0300

    Update Level2.java

--- Level2.java
+++ Level2.java
@@ -1,3 +1,4 @@
+
 package game.backend.level;
 import game.backend.GameState;
 import game.backend.element.CandyColor;
@@ -10,30 +11,39 @@ public class Level2 extends Level1 {
     private List<TimeBomb> bombs = new ArrayList<>();
     private int MIN_COUNT = 5, MAX_COUNT = 10;
     private Level2State gstate;
-    //private BombList bombs = new BombList();
-
-
 
     @Override
     public void initialize() {
         super.initialize(); //Hace lo mismo que antes solo que ahora reemplaza algunos elemenos por bombas de Tiempo
-        int amount = getRand(1,4);
+        int amount = getRand(1, 4);
         for (int j = 0; j <= amount; j++) {
             bombs.add(new TimeBomb(CandyColor.values()[getRand(0, CandyColor.values().length)], getRand(MIN_COUNT, MAX_COUNT)));
-
         }
 
         for (TimeBomb b : bombs) {
-            int i = getRand(0,SIZE-1);
-            int j = getRand(0,SIZE-1);
-            setContent(i, j,b);
-            tryRemove(g()[i][j]);
+            int i = getRand(0, SIZE - 1);
+            int j = getRand(0, SIZE - 1);
+            while((get(i,j) instanceof TimeBomb)) {
+                i = getRand(0, SIZE - 1);
+                j = getRand(0, SIZE - 1);
+            }
+            setContent(i, j, b);
 
         }
+        System.out.println("Antes de borrar hay " + bombsRemaining());
+        System.out.println("En el grid: "+bombsRemaining());
+        for(int i = 0; i< SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                tryRemove(g()[i][j]);
+                gstate.updateMinCounter();
+            }
+        }
         fallElements();
-        gstate.setBombs(bombs);
         gstate.updateMinCounter();
-        System.out.println(gstate.bombsRemaining());
+        System.out.println("Luego de borrar hay " + bombsRemaining());
+    }
+    public int bombsRemaining() {
+        return bombs.size();
     }
 
     @Override
@@ -41,29 +51,21 @@ public class Level2 extends Level1 {
         gstate = new Level2State(MAX_COUNT); //le mando el MAXCOUNT, porque despues lo voy a bajar. SI le mando el MINCOUNT, entonces puede que nunca lo baje y va a estar mal
         return gstate;
     }
-
     @Override
     public boolean tryMove(int i1, int j1, int i2, int j2) {
         boolean ret;
         if (ret = super.tryMove(i1, j1, i2, j2)) {
-            if(!gstate.getBombs().isEmpty())
+            if (!bombs.isEmpty())
                 gstate.decrementCounter();
         }
         return ret;
     }
 
-    @Override
-    public void clearContent(int i, int j) {
-        if(get(i,j) instanceof TimeBomb) {
-            TimeBomb bomb = (TimeBomb) get(i, j);
-            gstate.removeBomb(bomb);
-            bombs.remove(bomb);
-        }
-        g()[i][j].clearContent();
+    public List<TimeBomb> getBombs() {
+        return bombs;
     }
 
     private class Level2State extends GameState {
-        private List<TimeBomb> bombs;
         private int minCounter;
 
         Level2State(int minCounter) {
@@ -71,42 +73,22 @@ public class Level2 extends Level1 {
             this.minCounter = minCounter;
         }
 
-        public void setBombs(List<TimeBomb> bombs) {
-            this.bombs = bombs;
-        }
 
-        public List<TimeBomb> getBombs() {
-            return bombs;
-        }
-
-        public void removeBomb(TimeBomb bomb){
-            bombs.remove(bomb);
-            if(!bombs.isEmpty()){
-                updateMinCounter();
-            }else
-                minCounter = 0;
-        }
-
-        public void decrementCounter(){
-            for(TimeBomb b : bombs){
+        public void decrementCounter() {
+            for (TimeBomb b : bombs) {
                 b.decrementCounter();
             }
             minCounter--;
         }
 
-        public void updateMinCounter(){
-            for(TimeBomb b : bombs){
-                if(b.getCounter() < minCounter){
+        public void updateMinCounter() {
+            for (TimeBomb b : bombs) {
+                if (b.getCounter() < minCounter) {
                     minCounter = b.getCounter();
                 }
 
             }
         }
-
-        public int bombsRemaining(){
-            return bombs.size();
-        }
-
         @Override
         public boolean gameOver() {
             return playerWon() || minCounter == 0;
@@ -117,44 +99,4 @@ public class Level2 extends Level1 {
             return bombs.isEmpty();
         }
     }
-
-    /*private class BombList{
-        private List<TimeBomb> list = new ArrayList<>();
-        private int minCounter;
-        public void add(TimeBomb bomb){
-            if(list.isEmpty()) {
-                minCounter = bomb.getCounter();
-            }
-            list.add(bomb);
-            if(minCounter >= bomb.getCounter())
-                minCounter = bomb.getCounter();
-        }
-        public void remove(TimeBomb bomb){
-            list.remove(bomb);
-            updateMinCounter();
-            System.out.println("Removed" + " there are " + size() +" bombs left");
-            System.out.println("The minCounter is " + minCounter);
-        }
-        public void updateMinCounter(){
-            if(bombs.isEmpty())
-                minCounter = 0;
-            else {
-                minCounter = list.stream()
-                        .map(TimeBomb::getCounter)
-                        .min(Integer::compare).get();
-            }
-        }
-        public void decrementCounter(){
-            for(TimeBomb bomb : list) {
-                bomb.decrementCounter();
-            }
-            minCounter--;
-        }
-        public boolean isEmpty(){
-            return list.size() == 0;
-        }
-        public int size(){return list.size();}
-    }*/
-
-
 }
