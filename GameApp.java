af015e4b84926d2b5f1bf204dd02dabe8d1c87c0
JoacoGDR <45571740+JoacoGDR@users.noreply.github.com>
2019-07-06 22:30:11 -0300

    Add files via upload

--- GameApp.java
+++ GameApp.java
@@ -1,25 +1,21 @@
 package game.frontend;
 
-import game.backend.CandyGame;
 import game.backend.level.Level1;
 import game.backend.level.Level2;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import game.backend.CandyGame;
 import game.backend.level.Level3;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
-import java.util.Optional;
+
 
 public class GameApp extends Application {
 
-	Button b1 = new Button("Nivel 1");
-	Button b2 = new Button("Nivel 2");
-	Button b3 = new Button("Nivel 3");
 
 	public static void main(String[] args) {
 		launch(args);
@@ -27,47 +23,46 @@ public class GameApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-
-		TilePane r = new TilePane();
-
-
-		b1.setOnAction(e -> {CandyGame game = new CandyGame(Level1.class);
+		Button level1 = new Button("Level 1");
+		level1.setOnAction(event -> {CandyGame game = new CandyGame(Level1.class);
 			CandyFrame frame = new CandyFrame(game);
-			Scene scene = new Scene(frame);
 			primaryStage.setResizable(false);
+			Scene scene = new Scene(frame);
 			primaryStage.setScene(scene);
-			primaryStage.show();});
 
-		b2.setOnAction(e -> {CandyGame game = new CandyGame(Level2.class);
+		});
+		Button level2 = new Button("Level 2");
+		level2.setOnAction(event -> {CandyGame game = new CandyGame(Level2.class);
 			CandyFrame frame = new CandyFrame(game);
-			Scene scene = new Scene(frame);
 			primaryStage.setResizable(false);
+			Scene scene = new Scene(frame);
 			primaryStage.setScene(scene);
-			primaryStage.show();});
 
-		b3.setOnAction(e -> {CandyGame game = new CandyGame(Level3.class);
+		});
+		Button level3 = new Button("Level 3");
+		level3.setOnAction(event -> {CandyGame game = new CandyGame(Level3.class);
 			CandyFrame frame = new CandyFrame(game);
-			Scene scene = new Scene(frame);
 			primaryStage.setResizable(false);
+			Scene scene = new Scene(frame);
 			primaryStage.setScene(scene);
-			primaryStage.show();});
+		});
 
-		r.getChildren().addAll(b1,b2,b3);
-
-
-
-		//
-		Scene sc = new Scene(r, 200, 200);
-
-		// set the scene
-		primaryStage.setScene(sc);
-
+		VBox menu = new VBox(50);
+		menu.setAlignment(Pos.CENTER);
+		menu.getChildren().addAll(level1,level2,level3);
+		Image image = new Image("images/titleScreen.png");
+		Scene menuScene = new Scene(menu,image.getWidth(),image.getHeight());
+		BackgroundImage backgroundImage = new BackgroundImage(image,BackgroundRepeat.NO_REPEAT,
+				BackgroundRepeat.NO_REPEAT,
+				BackgroundPosition.DEFAULT,
+				BackgroundSize.DEFAULT);
+		Background background = new Background(backgroundImage);
+		menu.setBackground(background);
+		primaryStage.setResizable(false);
+		primaryStage.setScene(menuScene);
 		primaryStage.show();
 
-
 	}
-
-
 
 
 }
